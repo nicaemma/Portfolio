7c533b6d83fd3ab1f81fa241e5d65cea1e9c1f22
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-09 13:33:25 -0400

    Mobile Nav inital format complete

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiOutlineHome,
+  AiOutlineMail,
+  AiOutlineProject,
+} from "react-icons/ai";
+import { BsPerson, BsPersonWorkspace } from "react-icons/bs";
+import { GrProjects } from "react-icons/gr";
 
 const SideNav = () => {
   const [nav, setNav] = useState(false);
@@ -22,6 +29,34 @@ const SideNav = () => {
           >
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <GrProjects size={20} />
+            <span className="pl-4">Work</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineProject size={20} />
+            <span className="pl-4">Projects</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <BsPerson size={20} />
+            <span className="pl-4">Resume</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineMail size={20} />
+            <span className="pl-4">Contact</span>
           </a>
         </div>
       ) : (
