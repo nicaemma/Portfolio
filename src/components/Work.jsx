e09222bd9bf41bba7b71809ed4ad25c99df89cdd
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-10 16:38:28 -0400

    Responsive typo in Work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -26,7 +26,7 @@ const data = [
 
 function Work() {
   return (
-    <div id="work" className="max-w-[1040px] m-auto md:pl-21 p-4 py-16">
+    <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-[#001b5e] pb-6">
         Experience
       </h1>
