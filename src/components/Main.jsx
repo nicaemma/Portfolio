b8b4c392389e4b93c334d22d3e34817dfa755ad2
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-10 14:30:37 -0400

    Homepage formatted

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,52 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import {
+  FaInstagram,
+  FaLinkedinIn,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
 
 function Main() {
   return (
     <div id="main">
       <img
-        className="w-full h-screen object-cover object-left "
-        src="https://metro.co.uk/wp-content/uploads/2021/11/GettyImages-1215159894-1.jpg?quality=90&strip=all"
+        className="w-full h-screen object-cover object-left"
+        src="https://media.istockphoto.com/id/1295282802/vector/young-woman-is-sitting-at-a-computer-in-a-room-with-a-large-window-concept-of-remote-work.jpg?s=612x612&w=0&k=20&c=kT9_TlCVQ5fEHVa7oT_3gZ3z3wXddnypCVh8dvgUziw="
       ></img>
-      <div className="w-full h-screen absolute top-0 left-0 bg-white/50"></div>
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4xl text-gray-800 font-bold">
+            Nica Weisinger
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+            Rotating text--
+            <TypeAnimation
+              // play with options later --> https://react-type-animation.netlify.app/options
+              sequence={[
+                " This is the first",
+                1000,
+                " This is the second",
+                2000,
+              ]}
+              wrapper="div"
+              speed={50}
+              cursor={true}
+              style={{
+                // fontSize: "1em",
+                paddingLeft: "5x",
+                display: "block",
+              }}
+              repeat={Infinity}
+            />
+          </h2>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <FaLinkedinIn className="cursor-pointer" size={25} />
+            <FaGithub className="cursor-pointer" size={25} />
+            <FaInstagram className="cursor-pointer" size={25} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
@@ -15,3 +54,4 @@ function Main() {
 export default Main;
 
 // to flip bg image: scale-x-[-1]
+// alternative bg photo: "https://metro.co.uk/wp-content/uploads/2021/11/GettyImages-1215159894-1.jpg?quality=90&strip=all"
