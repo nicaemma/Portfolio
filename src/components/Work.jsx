b42f43644fd43bb32b4dd70f5b38825d6b44877f
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-10 15:07:26 -0400

    Wrote/formatted work page

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,12 @@ import Workplace from "./Workplace";
 
 const data = [
   {
+    year: 2022,
+    title: "Fullstack Academy Graduate",
+    duration: "6 Months",
+    details: "Summary of software engineering program.",
+  },
+  {
     year: 2021,
     title: "Math Teacher",
     duration: "1 Year",
@@ -16,16 +22,25 @@ const data = [
     details:
       "Led weekly meetings, delegated tasks, and coached teachers in their instruction. Served as lead instructor of general education and special education math classes.",
   },
-  {
-    year: 2022,
-    title: "Fullstack Academy Graduate",
-    duration: "6 Months",
-    details: "Summary of software engineering program.",
-  },
 ];
 
 function Work() {
-  return <div>Work</div>;
+  return (
+    <div id="work" className="max-w-[1040px] m-auto md:pl-21 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e] pb-6">
+        Experience
+      </h1>
+      {data.map((item, idx) => (
+        <Workplace
+          key={idx}
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Work;
