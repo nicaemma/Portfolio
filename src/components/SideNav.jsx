55c0ffc022dacf672226461bce6abd91a1443432
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-10 16:26:59 -0400

    Smooth scroll and fixed hrefs

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -63,25 +63,25 @@ const SideNav = () => {
         <div className="md:block hidden fixed top-[25%] z-10">
           <div className="flex flex-col">
             <a
-              href="main"
+              href="#main"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
             >
               <AiOutlineHome />
             </a>
             <a
-              href="work"
+              href="#work"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
             >
               <GrProjects />
             </a>
             <a
-              href="projects"
+              href="#projects"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
             >
               <AiOutlineProject />
             </a>
             <a
-              href="resume"
+              href="#resume"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
             >
               <BsPerson />
