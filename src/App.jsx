185cd023b08763d3131a8e644daa87c5947bf348
Nica Weisinger <nica.weisinger@gmail.com>
2023-06-10 14:47:43 -0400

    Setup work/experience components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import SideNav from "./components/SideNav";
 import Main from "./components/Main";
+import Work from "./components/Work";
 
 function App() {
   return (
     <>
       <SideNav />
       <Main />
+      <Work />
     </>
   );
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Workplace from "./Workplace";
+
+const data = [
+  {
+    year: 2021,
+    title: "Math Teacher",
+    duration: "1 Year",
+    details:
+      "Designed and implemented 6th and 7th grade math curriculums that successfully addressed pandemic-induced gaps in learning and social-emotional development while maintaining grade-level Common Core State Standards mastery.",
+  },
+  {
+    year: 2016,
+    title: "Math Teacher, Grade Team Lead, Instructional Coach",
+    duration: "5 Years",
+    details:
+      "Led weekly meetings, delegated tasks, and coached teachers in their instruction. Served as lead instructor of general education and special education math classes.",
+  },
+  {
+    year: 2022,
+    title: "Fullstack Academy Graduate",
+    duration: "6 Months",
+    details: "Summary of software engineering program.",
+  },
+];
+
+function Work() {
+  return <div>Work</div>;
+}
+
+export default Work;
